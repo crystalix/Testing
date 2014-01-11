3e768cb1a42ad67c77028f1ae861b1f7e8a994da
Tarnquil <Tarnquil@gmail.com>
2014-01-11 13:14:27 +0000

    Upgraded the project to the new unity version

--- Assets/scripts/plantScript.cs
+++ Assets/scripts/plantScript.cs
@@ -1,43 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class plantScript : MonoBehaviour 
+public class plantScript : MonoBehaviour
 {
-	public float growLength;
-	public string plantType;
+		//A penguin has editted a script
+		public float growLength;
+		public string plantType;
 		
-	public enum States
-	{
-		Unplanted,
-		Growing,
-		Ready
-	};
-	
-	public Texture2D[] textures;
-	public States state = States.Unplanted;
-
-	// Use this for initialization
-	void Start () 
-	{
-		plantType = "";
-		state = States.Unplanted;
-	}
-	
-	IEnumerator Growing()
-	{
-		state = States.Growing;
-		yield return new WaitForSeconds(growLength);
-		
-		switch(plantType)
+		public enum States
 		{
-			case "carrot":
-			this.gameObject.renderer.material.mainTexture = textures[4];break;
-			case "wheat":
-			this.gameObject.renderer.material.mainTexture = textures[5];break;
-			case"turnip":
-			this.gameObject.renderer.material.mainTexture = textures[5];break;
-		}
-		state = States.Ready;
+				Unplanted,
+				Growing,
+				Ready
 	}
+		;
+	
+		public Texture2D[] textures;
+		public States state = States.Unplanted;
+
+		// Use this for initialization
+		void Start ()
+		{
+				plantType = "";
+				state = States.Unplanted;
+		}
+	
+		IEnumerator Growing ()
+		{
+				state = States.Growing;
+				yield return new WaitForSeconds (growLength);
+		
+				switch (plantType) {
+				case "carrot":
+						this.gameObject.renderer.material.mainTexture = textures [4];
+						break;
+				case "wheat":
+						this.gameObject.renderer.material.mainTexture = textures [5];
+						break;
+				case"turnip":
+						this.gameObject.renderer.material.mainTexture = textures [5];
+						break;
+				}
+				state = States.Ready;
+		}
 	
 }
